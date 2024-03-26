1d24d3bdecc51a6ad2af039cfbac4dbc495d2022
Богданов Артём <bogdartem1997@yandex.ru>
2024-03-26 22:32:03 +0400

    Redacted by review and pep8

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 from django.core.management.utils import get_random_secret_key
 
+
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
